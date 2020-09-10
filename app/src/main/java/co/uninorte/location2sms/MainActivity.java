51385432aad9cbb83425fb568ed796e047c47970
Jesus Yepez <yepezdj2014@gmail.com>
2020-09-09 19:14:54 -0500

    Send location automatically

--- app/src/main/java/co/uninorte/location2sms/MainActivity.java
+++ app/src/main/java/co/uninorte/location2sms/MainActivity.java
@@ -2,10 +2,11 @@ package co.uninorte.location2sms;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -13,16 +14,12 @@ import android.text.Html;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -30,17 +27,17 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     //Initialize variable
     TextView textView1, textView2, textView3, textView4, textView5;
     FusedLocationProviderClient fusedLocationProviderClient;
+
     String txtMessage;
     EditText editip;
     @SuppressWarnings("deprecation")
     private Handler mHandler = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,53 +49,82 @@ public class MainActivity extends AppCompatActivity {
         textView1 = findViewById(R.id.text_view1);
         textView2 = findViewById(R.id.text_view2);
         textView3 = findViewById(R.id.text_view3);
-        textView4 = findViewById(R.id.text_view4);
-        textView5 = findViewById(R.id.text_view5);
+        //textView4 = findViewById(R.id.text_view4);
+        //textView5 = findViewById(R.id.text_view5);
         //cancel = (Button) findViewById(R.id.cancel);
         editip = findViewById(R.id.Edit_ip);
 
         //Initialize focusedLocationProviderClient
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(MainActivity.this
+                , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //When permission granted
+        } else {
+            //When permission denied
+            ActivityCompat.requestPermissions(MainActivity.this
+                    , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new MyLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 
-    //Envio periodico
+    private class MyLocationListener implements LocationListener {
+        @Override
+        @SuppressLint("SetTextI18n")
+        public void onLocationChanged(Location location) {
+            //Set latitude on TextView
+            textView1.setText(Html.fromHtml(
+                    "<font color= '#6200EE'><b>Latitud :</b><br></font>"
+                            + location.getLatitude()
+            ));
+            //Set longitude on TextView
+            textView2.setText(Html.fromHtml(
+                    "<font color= '#6200EE'><b>Longitud :</b><br></font>"
+                            + location.getLatitude()
+            ));
+            //Set time stamp
+            textView3.setText(Html.fromHtml(
+                    "<font color= '#6200EE'><b>TimeStamp :</b><br></font>"
+                            + new Date().toString()));
+            //Set message
+            txtMessage = ("\n" +
+                    "Latitud:_"+ location.getLatitude()+",\n"+
+                    "Longitud:_"+location.getLatitude()+",\n"+
+                    "TimeStamp:_"+new Date().toString());
+        }
+    }
+
+    public void stopRepeating(View v) {
+        mHandler.removeCallbacks(EnvioRunnable);
+    }
+
     public void startRepeating(View v) {
         //mHandler.postDelayed(mToastRunnable, 5000);
         EnvioRunnable.run();
     }
 
-    public void stopRepeating(View v) {
-        mHandler.removeCallbacks(EnvioRunnable);
-        enviarr();
-    }
+
 
     private final Runnable EnvioRunnable = new Runnable() {
         @Override
         public void run() {
-            if (ActivityCompat.checkSelfPermission(MainActivity.this
-                    , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                //When permission granted
-                getLocation();
-            } else {
-                //When permission denied
-                ActivityCompat.requestPermissions(MainActivity.this
-                        , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-            }
-
+            enviarr();
             mHandler.postDelayed(this, 5000);
-
         }
     };
 
-
-    @SuppressWarnings("deprecation")
-    public void enviarr()
-    {
+    
+    public void enviarr() {
+        //noinspection deprecation
         new UDPClient().execute(txtMessage);
     }
 
 
-    public void getLocation() {
+
+
+    /*public void getLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this
                 , Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             //getLocation();
@@ -140,14 +166,14 @@ public class MainActivity extends AppCompatActivity {
                         ));
                         //Set address
                         textView5.setText(Html.fromHtml(
-                                "<font color= '#6200EE'><b>Dirección :</b><br></font>"
-                                        + addresses.get(0).getAddressLine(0)
+                                "<font color= '#6200EE'><b>TimeStamp :</b><br></font>"
+                                        +new Date().toString()
                         ));
                         //Set message
-                        txtMessage  = ("\n" +
-                                "Latitud: " + addresses.get(0).getLatitude()+ ",\n" +
-                                "Longitud: "+ addresses.get(0).getLongitude() +",\n" +
-                                "TimeStamp: "+ new Date().toString());
+                        txtMessage = ("\n" +
+                                "Latitud: "+addresses.get(0).getLatitude()+",\n"+
+                                "Longitud: "+addresses.get(0).getLongitude()+",\n"+
+                                "TimeStamp: "+new Date().toString());
 
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -156,24 +182,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
+    }*/
 
 
     @SuppressLint("StaticFieldLeak")
     @SuppressWarnings("deprecation")
-    public class UDPClient  extends AsyncTask<String,Void,Void> {
+    private class UDPClient extends AsyncTask<String, Void, Void> {
 
         @Override
         protected Void doInBackground(String... voids) {
 
-            try{
+            try {
                 String message = voids[0];
 
-                InetAddress ip=InetAddress.getByName(editip.getText().toString());
-                DatagramSocket socket=new DatagramSocket();
+                InetAddress ip = InetAddress.getByName(editip.getText().toString());
+                DatagramSocket socket = new DatagramSocket();
                 byte[] outData = (message).getBytes();
 
-                DatagramPacket out = new DatagramPacket(outData,outData.length,ip ,11000);
+                DatagramPacket out = new DatagramPacket(outData, outData.length, ip, 11000);
                 socket.send(out);
 
                 //Toast.makeText(getApplicationContext(), "Send UDP", Toast.LENGTH_SHORT).show();
@@ -183,12 +209,13 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
             return null;
 
         }
     }
-
-
 }
+
+
 
 
