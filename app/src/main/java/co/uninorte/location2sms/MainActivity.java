3437ab5ab359c6057d438ca3e5b4da167febe888
Jesus Yepez <yepezdj2014@gmail.com>
2020-09-09 19:13:31 -0500

    With get and send buttons

--- app/src/main/java/co/uninorte/location2sms/MainActivity.java
+++ app/src/main/java/co/uninorte/location2sms/MainActivity.java
@@ -1,15 +1,16 @@
 package co.uninorte.location2sms;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Html;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,26 +25,22 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     //Initialize variable
-    Button btLocation;
     TextView textView1, textView2, textView3, textView4, textView5;
     FusedLocationProviderClient fusedLocationProviderClient;
     String txtMessage;
-    Button btnsnd2;
     EditText editip;
-
+    @SuppressWarnings("deprecation")
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,58 +48,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Assing variable
-        btLocation = findViewById(R.id.bt_location);
+        //btLocation = findViewById(R.id.bt_location);
         textView1 = findViewById(R.id.text_view1);
         textView2 = findViewById(R.id.text_view2);
         textView3 = findViewById(R.id.text_view3);
         textView4 = findViewById(R.id.text_view4);
         textView5 = findViewById(R.id.text_view5);
-        btnsnd2 = (Button) findViewById(R.id.btnSend2);
+        //cancel = (Button) findViewById(R.id.cancel);
         editip = findViewById(R.id.Edit_ip);
-
-
-
 
         //Initialize focusedLocationProviderClient
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
-        btLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Check permission
-                if (ActivityCompat.checkSelfPermission(MainActivity.this
-                        , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    //When permission granted
-                    getLocation();
-                } else {
-                    //When permission denied
-                    ActivityCompat.requestPermissions(MainActivity.this
-                            , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-                }
-            }
-        });
     }
+
+    //Envio periodico
+    public void startRepeating(View v) {
+        //mHandler.postDelayed(mToastRunnable, 5000);
+        EnvioRunnable.run();
+    }
+
+    public void stopRepeating(View v) {
+        mHandler.removeCallbacks(EnvioRunnable);
+        enviarr();
+    }
+
+    private final Runnable EnvioRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (ActivityCompat.checkSelfPermission(MainActivity.this
+                    , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                //When permission granted
+                getLocation();
+            } else {
+                //When permission denied
+                ActivityCompat.requestPermissions(MainActivity.this
+                        , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+            }
+
+            mHandler.postDelayed(this, 5000);
+
+        }
+    };
+
 
     @SuppressWarnings("deprecation")
-    public void enviarr(View v)
+    public void enviarr()
     {
-        EnviarTCP enviar = new EnviarTCP();
-        enviar.execute(txtMessage);
-
-        UDPClient enviaar = new UDPClient();
-        enviaar.execute(txtMessage);
-
-        //
+        new UDPClient().execute(txtMessage);
     }
 
 
-    private void getLocation() {
+    public void getLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this
                 , Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            getLocation();
+            //getLocation();
             return;
         }
-
 
         fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
             @Override
@@ -116,8 +117,7 @@ public class MainActivity extends AppCompatActivity {
                                 Locale.getDefault());
                         //Initialize address list
                         List<Address> addresses = geocoder.getFromLocation(
-                                location.getLatitude(), location.getLongitude(), 1
-                        );
+                                location.getLatitude(), location.getLongitude(), 1);
                         //Set latitude on TextView
                         textView1.setText(Html.fromHtml(
                                 "<font color= '#6200EE'><b>Latitud :</b><br></font>"
@@ -143,9 +143,10 @@ public class MainActivity extends AppCompatActivity {
                                 "<font color= '#6200EE'><b>Dirección :</b><br></font>"
                                         + addresses.get(0).getAddressLine(0)
                         ));
-                        //Set sms
-                        txtMessage  = ("Latitud: " + addresses.get(0).getLatitude()+ "\n" +
-                                "Longitud: "+ addresses.get(0).getLongitude() +"\n" +
+                        //Set message
+                        txtMessage  = ("\n" +
+                                "Latitud: " + addresses.get(0).getLatitude()+ ",\n" +
+                                "Longitud: "+ addresses.get(0).getLongitude() +",\n" +
                                 "TimeStamp: "+ new Date().toString());
 
                     } catch (IOException e) {
@@ -154,41 +155,11 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
     }
 
 
-    @SuppressWarnings("deprecation")
-    public class EnviarTCP extends AsyncTask<String,Void,Void>
-    {
-        Socket s;
-        PrintWriter pw;
-
-        @Override
-        protected Void doInBackground(String... voids) {
-
-            String message = voids[0];
-
-            try
-            {
-                s = new Socket(editip.getText().toString(), 10000);
-                pw= new PrintWriter(s.getOutputStream());
-                pw.write(message);
-                pw.flush();
-                pw.close();
-                s.close();
-
-
-            }catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-            return null;
-
-
-        }
-    }
-
-
+    @SuppressLint("StaticFieldLeak")
     @SuppressWarnings("deprecation")
     public class UDPClient  extends AsyncTask<String,Void,Void> {
 
@@ -215,9 +186,9 @@ public class MainActivity extends AppCompatActivity {
             return null;
 
         }
-
-
     }
+
+
 }
 
 
