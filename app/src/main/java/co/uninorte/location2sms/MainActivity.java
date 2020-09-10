9ae10cd9d1491ac651f158844888320dcdfb0fb8
Jesus Yepez <yepezdj2014@gmail.com>
2020-09-09 19:14:54 -0500

    With get and send buttons

--- app/src/main/java/co/uninorte/location2sms/MainActivity.java
+++ app/src/main/java/co/uninorte/location2sms/MainActivity.java
@@ -183,6 +183,7 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
             return null;
 
         }
